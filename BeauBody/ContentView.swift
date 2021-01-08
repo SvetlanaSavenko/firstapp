0acb0f767965d3f45789c5749f965bd990bd9b6b
Светлана Штыренкова <s.shtyrenkova@2gis.ru>
2021-01-08 14:32:16 +0700

    Initial Commit

---
+++ BeauBody/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeauBody
+//
+//  Created by Савенко Светлана Александровна on 08.01.2021.
+//  Copyright © 2021 Svetlana Savenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BeauBodyTests/BeauBodyTests.swift
@@ -0,0 +1,34 @@
+//
+//  BeauBodyTests.swift
+//  BeauBodyTests
+//
+//  Created by Савенко Светлана Александровна on 08.01.2021.
+//  Copyright © 2021 Svetlana Savenko. All rights reserved.
+//
+
+import XCTest
+@testable import BeauBody
+
+class BeauBodyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
